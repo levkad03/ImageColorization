e37e0d4e6f220f711ba896da0c5b3d6241fb8bf7
Lev Kadatskyi <levkadatscky@gmail.com>
2025-01-22 14:26:08 +0200

    added vae model and trying to make it work

--- train_vae.py
+++ train_vae.py
@@ -3,9 +3,14 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 
-from autoencoder import AutoEncoder
 from dataset import ColorizationDataset
-from utils import create_dataloader, load_checkpoint, save_checkpoint, save_predictions
+from utils import (
+    create_dataloader,
+    load_checkpoint,
+    save_checkpoint,
+    save_predictions_vae,
+)
+from vae import VariationalAutoEncoder, loss_function
 
 seed = 123
 
@@ -23,7 +28,7 @@ LOAD_MODEL = False
 OUTPUT_DIR = "predictions"
 
 
-def train_fn(loader, model, optimizer, loss_fn, scaler, epoch):
+def train_fn(loader, model, optimizer, scaler, epoch):
     loop = tqdm(loader, desc=f"Epoch: {epoch}")
     for batch_idx, (data, targets) in enumerate(loop):
         data = data.to(device=DEVICE)
@@ -31,8 +36,8 @@ def train_fn(loader, model, optimizer, loss_fn, scaler, epoch):
 
         # forward
         with torch.amp.autocast(DEVICE):
-            predictions = model(data)
-            loss = loss_fn(predictions, targets)
+            recon_batch, mu, log_var = model(data)
+            loss = loss_function(recon_batch, data, mu, log_var)
 
         # backward
         optimizer.zero_grad()
@@ -45,8 +50,7 @@ def train_fn(loader, model, optimizer, loss_fn, scaler, epoch):
 
 
 def main():
-    model = AutoEncoder().to(DEVICE)
-    loss_fn = nn.MSELoss()
+    model = VariationalAutoEncoder(latent_dim=2, in_channels=1).to(DEVICE)
     optimizer = optim.Adam(
         model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
     )
@@ -57,18 +61,18 @@ def main():
     train_loader, test_loader = create_dataloader(dataset, BATCH_SIZE)
 
     if LOAD_MODEL:
-        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+        load_checkpoint(torch.load("vae_checkpoint.pth.tar"), model)
 
     for epoch in range(EPOCHS):
-        train_fn(train_loader, model, optimizer, loss_fn, scaler, epoch)
+        train_fn(train_loader, model, optimizer, scaler, epoch)
 
         checkpoint = {
             "state_dict": model.state_dict(),
             "optimizer": optimizer.state_dict(),
         }
-        save_checkpoint(checkpoint)
+        save_checkpoint(checkpoint, filename="vae_checkpoint.pth.tar")
 
-        save_predictions(model, test_loader, OUTPUT_DIR, DEVICE, n_samples=20)
+        save_predictions_vae(model, test_loader, OUTPUT_DIR, DEVICE, n_samples=20)
 
 
 if __name__ == "__main__":
---
+++ vae.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VariationalAutoEncoder(nn.Module):
+    def __init__(self, latent_dim, in_channels=1):
+        super(VariationalAutoEncoder, self).__init__()
+        self.latent_dim = latent_dim
+
+        # encoder
+        self.encoder = nn.Sequential(
+            # first conv
+            nn.Conv2d(in_channels, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # second conv
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # third conv
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        # latent space
+        self.fc_mu = nn.Linear(128 * 32 * 32, latent_dim)
+        self.fc_gvar = nn.Linear(128 * 32 * 32, latent_dim)
+
+        self.decoder_input = nn.Linear(latent_dim, 128 * 32 * 32)
+
+        self.decoder = nn.Sequential(
+            # first deconv
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(),
+            # second deconv
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+            nn.ReLU(),
+            # third deconv
+            nn.ConvTranspose2d(32, 3, kernel_size=2, stride=2),
+            nn.ReLU(),
+        )
+
+    def encode(self, x):
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        mu = self.fc_mu(x)
+        gvar = self.fc_gvar(x)
+        return mu, gvar
+
+    def reparametrize(self, mu, gvar):
+        std = torch.exp(0.5 * gvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        z = self.decoder_input(z)
+        z = z.view(-1, 128, 32, 32)
+        return self.decoder(z)
+
+    def forward(self, x):
+        mu, gvar = self.encode(x)
+        z = self.reparametrize(mu, gvar)
+        return self.decode(z), mu, gvar
+
+
+def loss_function(recon_x, x, mu, log_var):
+    # Reconstruction loss
+    MSE = F.mse_loss(recon_x, x, reduction="sum")
+
+    # KL divergence loss
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+
+    return MSE + KLD
