532994cb87d8ccad25ed8a7d84692355e3850d5b
Lev Kadatskyi <levkadatscky@gmail.com>
2025-02-11 13:08:53 +0200

    deleted VAE cuz it didn't work

--- utils.py
+++ utils.py
@@ -49,40 +49,6 @@ def save_predictions(model, loader, output_dir, device, n_samples=10):
     model.train()
 
 
-def save_predictions_vae(model, loader, output_dir, device, n_samples=10):
-    os.makedirs(output_dir, exist_ok=True)
-
-    saved_count = 0
-
-    model.eval()
-    with torch.no_grad():
-        for idx, (gray, original) in enumerate(loader):
-            if saved_count >= n_samples:
-                break
-
-            gray = gray.to(device=device)
-            predictions, mu, gvar = model(gray)
-
-            for i in range(gray.shape[0]):
-                if saved_count >= n_samples:
-                    break
-
-                pred_img = predictions[i].cpu().numpy()
-                img_name = f"prediction_{idx}_{i}.jpg"
-
-                pred_img = torch.from_numpy(pred_img).float().cpu()
-
-                save_image(
-                    original[i], os.path.join(output_dir, f"original_{idx}_{i}.jpg")
-                )
-
-                save_image(pred_img, os.path.join(output_dir, img_name))
-
-                saved_count += 1
-
-    model.train()
-
-
 def create_dataloader(
     dataset,
     batch_size=32,
